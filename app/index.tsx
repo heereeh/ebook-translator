4b3d0b46cc6cdce18ea934c9a8931b9c20eaac49
heereeh <rodbee20@gmail.com>
2025-08-17 16:51:33 +0900

    [Feat] 프로젝트 셋팅

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-3xl font-bold">My first App!</Text>
+    </View>
+  );
+}
